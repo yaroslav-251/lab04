9f24b8b7750d638bb63341901dc5937622dcf185
yaroslav <kolpakov1803@mail.ru>
2026-09-15 11:13:50 +0300

    Lab04: Logical Operators, Practice

--- Program.cs
+++ Program.cs
@@ -10,3 +10,40 @@ else
 }
 Console.WriteLine("Программа продолжает работу");
 
+int ade = 85;
+if (ade < 13)
+{
+    Console.WriteLine("Ребенок");
+}
+else if (ade < 18)
+{
+    Console.WriteLine("Подросток");
+}
+else if (ade >= 60)
+{
+    Console.WriteLine("Пенсионер");
+}
+else
+{
+    Console.WriteLine("Взрослый");
+}
+
+int ages = 16;
+double height = 1.55;
+bool razresh = true;
+if (ages >= 14 && height >= 1.5)
+{
+    Console.WriteLine("Можно кататься");
+}
+else if (razresh == true) 
+{
+    Console.WriteLine("кататься можно");
+}
+
+
+else
+{
+    Console.WriteLine("Пока нельзя");
+}
+
+
